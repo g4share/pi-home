b7797af8eaeff2b4d92f735da1531f474842cbb6
g4share <g4share@gmail.com>
2024-02-20 18:43:50 +0200

    add pi hole 4

--- files/2-samba.sh
+++ files/2-samba.sh
@@ -6,12 +6,14 @@ sudo mkdir -p /mnt/data/shared
 sudo chmod 777 -R /mnt/data/shared
 
 if ! grep -q '\[shared\]' /etc/samba/smb.conf ; then
-    echo '[shared]'                >> /etc/samba/smb.conf
-    echo 'path = /mnt/data/shared' >> /etc/samba/smb.conf
-    echo 'writeable=Yes'           >> /etc/samba/smb.conf
-    echo 'create mask=0666'        >> /etc/samba/smb.conf
-    echo 'directory mask=0777'     >> /etc/samba/smb.conf
-    echo 'public=Yes'              >> /etc/samba/smb.conf
+    {
+        echo '[shared]'
+        echo 'path = /mnt/data/shared'
+        echo 'writeable=Yes'
+        echo 'create mask=0666'
+        echo 'directory mask=0777'
+        echo 'public=Yes'
+    } >> /etc/samba/smb.conf
 
     sudo systemctl restart smbd
 fi
--- files/3-portainer.sh
+++ files/3-portainer.sh
@@ -3,6 +3,7 @@
 docker pull portainer/portainer-ce:latest
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+#do not create folders on rpi4 - it's just for Pi Hole
 mkdir -p /mnt/data/volumes/torrent/config
 mkdir -p /mnt/data/volumes/torrent/data
 
